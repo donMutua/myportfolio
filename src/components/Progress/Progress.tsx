6e7e74c6cd30ca117111ac0818c9bff37b515858
don.technologist <don.technologist@gmail.com>
2024-08-12 00:21:14 +0300

    disable progress

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,30 +1,7 @@
-// import React from "react";
-// import * as Progress from "@radix-ui/react-progress";
+import React from "react";
 
-// type SkillProps = {
-//   skill: {
-//     language: string;
-//     skill: number;
-//   };
-// };
+function Progress() {
+  return <div>Progress</div>;
+}
 
-// function ProgressComponent({ skill }: SkillProps) {
-//   const skillPercentage = skill?.skill * 100;
-
-//   console.log(typeof skillPercentage, skillPercentage);
-
-//   return (
-//     <div className="flex flex-col md:items-center md:space-x-4 m-auto md:flex-row ">
-//       <span>{skill.language}:</span>
-//       <Progress.Root className="h-4 w-80 bg-gray-200 rounded-full" max={100}>
-//         <Progress.Indicator
-//           className="h-4 bg-black rounded-full"
-//           style={{ width: `${skillPercentage}%` }}
-//         />
-//       </Progress.Root>
-//       {/* <span>{skillPercentage}%</span> */}
-//     </div>
-//   );
-// }
-
-// export default ProgressComponent;
+export default Progress;
