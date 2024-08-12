21c38583ab5ba1023ea25a5328599d13da7ca1b1
don.technologist <don.technologist@gmail.com>
2024-08-12 17:21:28 +0300

    ui fixes

--- src/components/Skills/SkillsSection.tsx
+++ src/components/Skills/SkillsSection.tsx
@@ -3,13 +3,12 @@ import { Skill } from "@/lib/api";
 import ProgressComponent from "../Progress/Progress";
 
 function SkillsSection({ skills = [] }: { skills?: Skill[] }) {
-  const skillsProp = skills || [];
   return (
     <section id="skills" className=" p-5 md:pt-10 md:pb-20 bg-white">
       <h1 className="font-semibold text-2xl mb-2 text-center">Skills</h1>
 
       <div className="flex flex-wrap gap-6 md:grid md:grid-cols-2">
-        {skillsProp?.map((skill) => (
+        {skills?.map((skill) => (
           <ProgressComponent key={skill.id} skill={skill} />
         ))}
       </div>
