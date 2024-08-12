d9157ef36dabb3f33f979534946f92c39b1cedcf
don.technologist <don.technologist@gmail.com>
2024-08-12 17:29:47 +0300

    fix: check skill perrcentage

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -15,13 +15,17 @@ function ProgressComponent({ skill }: SkillProps) {
     <div className="flex flex-col md:items-center md:space-x-4 m-auto md:flex-row ">
       <span>{skill?.language}:</span>
       <Progress.Root className="h-4 w-80 bg-gray-200 rounded-full" max={100}>
-        <Progress.Indicator
-          className="h-4 bg-black rounded-full"
-          style={{ width: `${Number(skillPercentage)}%` }}
-        ></Progress.Indicator>
+        <Progress.Indicator style={{ width: `${Number(skillPercentage)}%` }} />
       </Progress.Root>
     </div>
   );
 }
 
 export default ProgressComponent;
+
+{
+  /* <Progress.Indicator
+className="h-4 bg-black rounded-full"
+style={{ width: `${Number(skillPercentage)}%` }}
+/> */
+}
