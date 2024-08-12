b1b79b4e0818b0113c4d7555d6b2fdbb0455c397
don.technologist <don.technologist@gmail.com>
2024-08-12 16:51:23 +0300

    Fix: progress bar

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import * as Progress from "@radix-ui/react-progress";
 
-function Progress() {
-  return <div>Progress</div>;
+type SkillProps = {
+  skill: {
+    language: string;
+    skill: number;
+  };
+};
+
+function ProgressComponent({ skill }: SkillProps) {
+  const skillPercentage = skill && skill?.skill * 100;
+
+  console.log(typeof skillPercentage, skillPercentage);
+
+  return (
+    <div className="flex flex-col md:items-center md:space-x-4 m-auto md:flex-row ">
+      <span>{skill?.language}:</span>
+      <Progress.Root className="h-4 w-80 bg-gray-200 rounded-full" max={100}>
+        <Progress.Indicator
+          className="h-4 bg-black rounded-full"
+          style={{ width: `${Number(skillPercentage)}%` }}
+        />
+      </Progress.Root>
+    </div>
+  );
 }
 
-export default Progress;
+export default ProgressComponent;
--- src/components/Skills/SkillsSection.tsx
+++ src/components/Skills/SkillsSection.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Skill } from "@/lib/api";
-// import ProgressComponent from "../Progress/Progress";
+import ProgressComponent from "../Progress/Progress";
 
 function SkillsSection({ skills = [] }: { skills?: Skill[] }) {
+  const skillsProp = skills || [];
   return (
     <section id="skills" className=" p-5 md:pt-10 md:pb-20 bg-white">
       <h1 className="font-semibold text-2xl mb-2 text-center">Skills</h1>
 
       <div className="flex flex-wrap gap-6 md:grid md:grid-cols-2">
-        {/* {skills?.map((skill) => (
+        {skillsProp?.map((skill) => (
           <ProgressComponent key={skill.id} skill={skill} />
-        ))} */}
+        ))}
       </div>
     </section>
   );
